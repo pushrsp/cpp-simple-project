0aefa3977fa2a77263732ca15b6db30d628a03cc
pushrsp@gmail.com <pushrsp@gmail.com>
2022-03-13 19:37:01 +0900

    ADD: star 2

--- start2/main.cpp
+++ start2/main.cpp
@@ -4,11 +4,10 @@ using namespace std;
 
 int main() {
     int count;
-
     cin >> count;
 
     for (int i = 0; i < count; i++) {
-        for (int j = 0; j < count; j++) cout << "*";
+        for (int j = 0; j < i + 1; j++) cout << "*";
         cout << endl;
     }
 
